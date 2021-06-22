02627ff489cd6956732329b61d82342246835102
klukovka <yuliia.koba@nure.ua>
2021-06-23 00:58:54 +0300

    add cheking todo

--- lib/widgets/todo_line.dart
+++ lib/widgets/todo_line.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:just_do_it/models/todo.dart';
+import 'package:just_do_it/services/firestore_service.dart';
 
 class ToDoLine extends StatelessWidget {
   ToDo todo;
@@ -8,15 +9,25 @@ class ToDoLine extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final firestore = FirestoreService();
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: Card(
         color: todo.todoColor.withOpacity(0.8),
         elevation: 5,
-            child: ListTile(
+        child: ListTile(
           title: Text('${todo.title}'),
           subtitle: Text('${todo.description}'),
-         
+          trailing: todo.done == false
+              ? Icon(Icons.check_box_outline_blank)
+              : Icon(Icons.check_box),
+          onTap: () {
+            if (todo.done == false)
+              todo.done = true;
+            else
+              todo.done = false;
+            firestore.saveToDo(todo);
+          },
         ),
       ),
     );
